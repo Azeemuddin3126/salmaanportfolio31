ad4174681aafee1cbf5ea172be69a9de391bb07b
Azeemuddin3126 <sonuhulk8765@gmail.com>
2022-10-19 20:54:13 +0530

    67

--- app/urls.py
+++ app/urls.py
@@ -1,12 +1,12 @@
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
 from app import views
-from django.conf import settings
-from django.conf.urls.static import static
+from django.views.static import serve
+
 
 
 
 
 urlpatterns = [
     path('', views.home, name='home'),
-]+ static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT) +  static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+]
