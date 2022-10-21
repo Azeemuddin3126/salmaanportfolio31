c52b5525e7df5804cc9a67548fd62d3b58d43fed
Azeemuddin3126 <sonuhulk8765@gmail.com>
2022-10-21 11:26:15 +0530

    ed

--- app/views.py
+++ app/views.py
@@ -34,7 +34,7 @@ def home(request):
     exp = experience.objects.all()
     skilll = skill.objects.all()
     res = resume.objects.all()
-    proj = projects.objects.all().order_by('-id')
+    proj = projects.objects.order_by('-id').all()
     sl = social.objects.all()
     cs = certificate.objects.all()
     ah = achievements.objects.all()
