e5b27b4855f221184040c92db271403154e4d256
Azeemuddin3126 <sonuhulk8765@gmail.com>
2022-10-21 10:05:23 +0530

    commity55

--- Main/settings.py
+++ Main/settings.py
@@ -137,8 +137,6 @@ STATIC_ROOT=os.path.join(BASE_DIR,'static')
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR,'media')
-DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
-
 
 CLOUDINARY_STORAGE = {
     'CLOUD_NAME': 'dhbzkbgo0',
