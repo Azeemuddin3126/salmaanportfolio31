40d35d438ee63cdc5deac36193ab4e998c120e17
Azeemuddin3126 <sonuhulk8765@gmail.com>
2022-10-23 22:26:05 +0530

    Update settings.py

--- Main/settings.py
+++ Main/settings.py
@@ -27,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-2&!*^y25rz#+ga=(m3#s9b7(mtg_u=yp6qc#0z$)xof(5b6b+3'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 
 
