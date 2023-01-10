afa36b1e108ffb18cb215e568c54ca04de453eda
Azeemuddin3126 <sonuhulk8765@gmail.com>
2023-01-10 12:01:48 +0530

    d

--- Main/settings.py
+++ Main/settings.py
@@ -97,10 +97,12 @@ WSGI_APPLICATION = 'Main.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'postgres',
         'USER': 'postgres',
         'PASSWORD': 'salmaan224',
+        'HOST': '127.0.0.1',
+        'PORT': 5432,
     }
 }
 
