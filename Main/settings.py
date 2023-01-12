bc46a78a2d00df22e7c6a07ca1d73e0119cb1d36
Azeemuddin3126 <sonuhulk8765@gmail.com>
2023-01-12 15:55:08 +0530

    d

--- Main/settings.py
+++ Main/settings.py
@@ -87,25 +87,25 @@ WSGI_APPLICATION = 'Main.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
 
 
 
 #mysql -h 127.0.0.1 -P 3307 -u root -p portsal
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'salmaan',
-        'USER': 'salmaan',
-        'PASSWORD': 1234,
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'salmaan',
+#         'USER': 'salmaan',
+#         'PASSWORD': 1234,
+#     }
+# }
 
 
 # Password validation
